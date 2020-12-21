5e9904a2a15bc8cf4708dc5ba7f61db9965ff9e9
Jude Francis <judecodes@gmail.com>
2020-12-21 01:33:27 +0100

    fix: eslint warnings

--- src/models/university.js
+++ src/models/university.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose');
 
 const universitySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    logo: String,
-    rank: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    acronym: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true
-    },
-    motto: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    longitude: {
-        type: Number,
-        required: true
-    },
-    latitude: {
-        type: Number,
-        required: true
-    },
-    media: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  logo: String,
+  rank: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  acronym: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  motto: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  media: {
+    type: String,
+    required: true,
+  },
 });
 
 const University = mongoose.model('University', universitySchema);
