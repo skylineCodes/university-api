85cc08fca89a49030abf7a8996705b7210e1f2b3
Jude Francis <judecodes@gmail.com>
2021-01-01 02:17:36 +0100

    chore: basic routing setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import './db/connection';
+import api from './api';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api', api);
 
 export default app;
