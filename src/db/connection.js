ea20f91515f349fbd30bec0f312f011068a356d2
Jude Francis <judecodes@gmail.com>
2021-01-01 02:03:55 +0100

    chore: setup debug for console logging

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,15 +1,10 @@
 import mongoose from 'mongoose';
+import debug from '../config/debug';
 
 mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 })
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('Database connection successful!');
-  })
-  .catch((err) => {
-    // eslint-disable-next-line no-console
-    console.log('Unable to connect to the mongodb instance. Error: ', err);
-  });
+  .then(() => debug.log('Database connection successful!'))
+  .catch((err) => debug.log('Unable to connect to the mongodb instance. Error: ', err));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import app from './app';
+import debug from './config/debug';
 
 /**
  * Start up server
  */
 app.listen(process.env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is up on port ${process.env.PORT}`);
+  debug.log(`Server is up on port ${ process.env.PORT }`);
 });
