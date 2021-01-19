224c7afc2377649de5574bd5aa8f6605918345b4
Jude Francis <judecodes@gmail.com>
2021-01-19 10:21:08 +0100

    chore: replace babel to default nodejs modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import path from 'path';
 import app from './app';
 import debug from './config/debug';
+
+// eslint-disable-next-line no-underscore-dangle
+global.__dirname = path.resolve('./');
 
 /**
  * Start up server
